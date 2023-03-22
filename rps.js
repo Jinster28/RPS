7abe26afc0db6ecbd55c9001d0bb2ba2d4858ae8
Simon Jin <jinster28@gmail.com>
2023-03-21 22:43:19 -0400

    Updated getComputerChoice() function on rps.js.  This function randomly generates one of Rock, Paper, or Scissors

---
+++ rps.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    let randNum = Math.floor(Math.random() * 3);
+
+    if (randNum == 0) {
+        return 'Rock';
+    }
+    else if (randNum == 1) {
+        return 'Paper';
+    }
+    else {
+        return 'Scissor';
+    }
+}
+
+getComputerChoice();
