ef13c4eb66ad727bb1da7836e22ec48345ad0308
Simon Jin <jinster28@gmail.com>
2023-03-22 23:31:24 -0400

    Added some code to playerSelection() function

--- rps.js
+++ rps.js
@@ -12,10 +12,19 @@ function getComputerChoice() {
     }
 }
 
-// function playerSelection()
 // Takes user input
+function playerSelection() {
+    
+    let userInput = prompt("What is your move?  Please enter one of Rock, Paper, or Scissor").toUpperCase();       
+     
+    console.log(userInput);
+
+    return userInput;
+}
+
 
 // function playRound(playerSelection(), getComputerChoice())
 // simulate a game of Rock Paper Scissors while taking 2 parameters
 
-getComputerChoice();
+//getComputerChoice();
+playerSelection();
