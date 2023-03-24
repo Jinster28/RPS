dea6dc46dabcdc8dc30e3357f16e0a2940596174
Simon Jin <jinster28@gmail.com>
2023-03-24 13:09:29 -0400

    Updated with working playRound() function

--- rps.js
+++ rps.js
@@ -18,18 +18,47 @@ function playerSelection() {
     let userInput = prompt("What is your move?  Please enter one of Rock, Paper, or Scissor").toUpperCase();       
      
     while (userInput != "ROCK" && userInput != "SCISSOR" && userInput != "PAPER") {
-        userInput = prompt("Invalid choice.  Please enter one of Rock, Paper, or Scissor").toUpperCase();
-        console.log(userInput);
+        userInput = prompt("Invalid choice.  Please enter one of Rock, Paper, or Scissor").toUpperCase(); 
     }
-
-    console.log(userInput);
 
     return userInput;
 }
 
+// simulate a game of Rock Paper Scissors while taking 2 parameters.  First is player choice, second is computer generated choice
+function playRound(playerChoice, computerChoice) {
+    let ps = playerChoice.toUpperCase();
+    let cs = computerChoice.toUpperCase();
 
-// function playRound(playerSelection(), getComputerChoice())
-// simulate a game of Rock Paper Scissors while taking 2 parameters
+    if (ps == cs) {
+        console.log("Draw");
+    }
+    else if (ps == "ROCK") {
+        if (cs == "PAPER") {
+            console.log("You Lose!  Paper beats Rock!");
+        }
+        else if (cs == "SCISSOR") {
+            console.log("You Win!  Rock beats Scissor!");
+        }
+    }
+    else if (ps == "PAPER") {
+        if (cs == "SCISSOR") {
+            console.log("You Lose!  Scissor beats Paper!");
+        }
+        else if (cs == "ROCK") {
+            console.log("You Win!  Paper beats Rock!");
+        }
+    }
+    else if (ps == "SCISSOR") {
+        if (cs == "ROCK") {
+            console.log("You Lose!  Rock beats Scissor!");
+        }
+        else if (cs == "PAPER") {
+            console.log("You Win!  Scissor beats Paper!");
+        }
+    }
+}
+
 
 //getComputerChoice();
 //playerSelection();
+playRound(playerSelection(), getComputerChoice());
